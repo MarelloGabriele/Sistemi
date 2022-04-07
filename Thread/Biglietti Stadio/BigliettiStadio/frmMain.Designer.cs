7a0723f0f0192ab20062c2bab7a8e589a7bab855
Gabriele <marellogabriele@gmail.com>
2022-04-07 13:24:54 +0200

    Upload esercitazione biglietteria
    Esercitazione relativa alla gestione di uno stadio composto da 4 biglietterie. Viene gestita la vendita dei biglietti ai clienti evitando duplicati. Viene gestito anche un potenziale attacco terroristico

---
+++ Thread/Biglietti Stadio/BigliettiStadio/frmMain.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace BigliettiStadio
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnAvvia = new System.Windows.Forms.Button();
+            this.txtPostiDisponibili = new System.Windows.Forms.TextBox();
+            this.txtTotClienti = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelN = new System.Windows.Forms.Label();
+            this.labelSud = new System.Windows.Forms.Label();
+            this.labelE = new System.Windows.Forms.Label();
+            this.labelO = new System.Windows.Forms.Label();
+            this.labelPosti = new System.Windows.Forms.Label();
+            this.lab0 = new System.Windows.Forms.Label();
+            this.lab1 = new System.Windows.Forms.Label();
+            this.lab2 = new System.Windows.Forms.Label();
+            this.lab3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAvvia
+            // 
+            this.btnAvvia.Location = new System.Drawing.Point(1025, 48);
+            this.btnAvvia.Margin = new System.Windows.Forms.Padding(7);
+            this.btnAvvia.Name = "btnAvvia";
+            this.btnAvvia.Size = new System.Drawing.Size(222, 95);
+            this.btnAvvia.TabIndex = 0;
+            this.btnAvvia.Text = "AVVIA ACQUISTO";
+            this.btnAvvia.UseVisualStyleBackColor = true;
+            this.btnAvvia.Click += new System.EventHandler(this.BtnAvvia_Click);
+            // 
+            // txtPostiDisponibili
+            // 
+            this.txtPostiDisponibili.Location = new System.Drawing.Point(1164, 277);
+            this.txtPostiDisponibili.Margin = new System.Windows.Forms.Padding(7);
+            this.txtPostiDisponibili.Name = "txtPostiDisponibili";
+            this.txtPostiDisponibili.Size = new System.Drawing.Size(152, 35);
+            this.txtPostiDisponibili.TabIndex = 1;
+            this.txtPostiDisponibili.Text = "100";
+            this.txtPostiDisponibili.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTotClienti
+            // 
+            this.txtTotClienti.Location = new System.Drawing.Point(1164, 334);
+            this.txtTotClienti.Margin = new System.Windows.Forms.Padding(7);
+            this.txtTotClienti.Name = "txtTotClienti";
+            this.txtTotClienti.Size = new System.Drawing.Size(152, 35);
+            this.txtTotClienti.TabIndex = 2;
+            this.txtTotClienti.Text = "70";
+            this.txtTotClienti.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(975, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "POSTI DISPONIBILI:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(974, 340);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "CLIENTI:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(976, 206);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(72, 18);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "[lblStatus]";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(35, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(921, 632);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.Location = new System.Drawing.Point(500, 133);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(0, 29);
+            this.labelN.TabIndex = 7;
+            // 
+            // labelSud
+            // 
+            this.labelSud.AutoSize = true;
+            this.labelSud.Location = new System.Drawing.Point(500, 459);
+            this.labelSud.Name = "labelSud";
+            this.labelSud.Size = new System.Drawing.Size(0, 29);
+            this.labelSud.TabIndex = 8;
+            // 
+            // labelE
+            // 
+            this.labelE.AutoSize = true;
+            this.labelE.Location = new System.Drawing.Point(701, 299);
+            this.labelE.Name = "labelE";
+            this.labelE.Size = new System.Drawing.Size(0, 29);
+            this.labelE.TabIndex = 9;
+            // 
+            // labelO
+            // 
+            this.labelO.AutoSize = true;
+            this.labelO.Location = new System.Drawing.Point(275, 299);
+            this.labelO.Name = "labelO";
+            this.labelO.Size = new System.Drawing.Size(0, 29);
+            this.labelO.TabIndex = 10;
+            // 
+            // labelPosti
+            // 
+            this.labelPosti.AutoSize = true;
+            this.labelPosti.Location = new System.Drawing.Point(835, 48);
+            this.labelPosti.Name = "labelPosti";
+            this.labelPosti.Size = new System.Drawing.Size(0, 29);
+            this.labelPosti.TabIndex = 11;
+            // 
+            // lab0
+            // 
+            this.lab0.AutoSize = true;
+            this.lab0.Location = new System.Drawing.Point(409, 133);
+            this.lab0.Name = "lab0";
+            this.lab0.Size = new System.Drawing.Size(73, 29);
+            this.lab0.TabIndex = 12;
+            this.lab0.Text = "Nord:";
+            this.lab0.Click += new System.EventHandler(this.lblClick);
+            // 
+            // lab1
+            // 
+            this.lab1.AutoSize = true;
+            this.lab1.Location = new System.Drawing.Point(642, 299);
+            this.lab1.Name = "lab1";
+            this.lab1.Size = new System.Drawing.Size(53, 29);
+            this.lab1.TabIndex = 13;
+            this.lab1.Text = "Est:";
+            this.lab1.Click += new System.EventHandler(this.lblClick);
+            // 
+            // lab2
+            // 
+            this.lab2.AutoSize = true;
+            this.lab2.Location = new System.Drawing.Point(409, 459);
+            this.lab2.Name = "lab2";
+            this.lab2.Size = new System.Drawing.Size(62, 29);
+            this.lab2.TabIndex = 14;
+            this.lab2.Text = "Sud:";
+            this.lab2.Click += new System.EventHandler(this.lblClick);
+            // 
+            // lab3
+            // 
+            this.lab3.AutoSize = true;
+            this.lab3.Location = new System.Drawing.Point(188, 299);
+            this.lab3.Name = "lab3";
+            this.lab3.Size = new System.Drawing.Size(81, 29);
+            this.lab3.TabIndex = 15;
+            this.lab3.Text = "Ovest:";
+            this.lab3.Click += new System.EventHandler(this.lblClick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(642, 48);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(185, 29);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Biglietti Rimasti:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1010, 580);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(283, 79);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "ATTENTATO!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1345, 671);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lab3);
+            this.Controls.Add(this.lab2);
+            this.Controls.Add(this.lab1);
+            this.Controls.Add(this.lab0);
+            this.Controls.Add(this.labelPosti);
+            this.Controls.Add(this.labelO);
+            this.Controls.Add(this.labelE);
+            this.Controls.Add(this.labelSud);
+            this.Controls.Add(this.labelN);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTotClienti);
+            this.Controls.Add(this.txtPostiDisponibili);
+            this.Controls.Add(this.btnAvvia);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VENDITA BIGLIETTI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAvvia;
+        private System.Windows.Forms.TextBox txtPostiDisponibili;
+        private System.Windows.Forms.TextBox txtTotClienti;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.Label labelSud;
+        private System.Windows.Forms.Label labelE;
+        private System.Windows.Forms.Label labelO;
+        private System.Windows.Forms.Label labelPosti;
+        private System.Windows.Forms.Label lab0;
+        private System.Windows.Forms.Label lab1;
+        private System.Windows.Forms.Label lab2;
+        private System.Windows.Forms.Label lab3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Thread/Biglietti Stadio/BigliettiStadio/frmMain.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace BigliettiStadio
+{
+    public partial class frmMain : Form
+    {
+        int[] vet;
+        bool isAttentato = false;
+        String[] log = new String[4]; //0Nord, 1Sud, 2Est, 3Ovest
+        //log[idBiglietteria] += "Cliente: " + cli[pos].idCliente + " ha acquistato un biglietto in posizione: " + postiStadio[rndPos].idPosto.toString();
+        const int BIGLIETTERIE = 4;
+        object lock_campo = new object();
+        object lock_biglietti = new object();
+        int totClienti, totPosti;
+        volatile Random rnd;
+        Label[] labels = new Label[4];
+        Thread[] biglietteria;
+
+        public struct Clienti
+        {
+            public int idCliente;
+            public int idBiglietteria; //0Nord, 1Sud, 2Est, 3Ovest
+            public bool acquistato;//acquistato= transizione eseguita o meno
+            public bool smaltito;//true= se ha cercato di acquistare
+        }
+        volatile Clienti[] cli;
+
+        public struct PostiStadio
+        {
+            public int idPosto;
+            public bool occupato;
+            public int idCliente;
+        }
+        PostiStadio[] postiStadio;
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+            labels[0] = labelN;
+            labels[1] = labelSud;
+            labels[2] = labelE;
+            labels[3] = labelO;
+        }
+        private void BtnAvvia_Click(object sender, EventArgs e)
+        {
+            Thread t;
+            rnd = new Random();
+            totClienti = Convert.ToInt32(txtTotClienti.Text);
+            totPosti = Convert.ToInt32(txtPostiDisponibili.Text);
+
+            cli = new Clienti[totClienti];
+            postiStadio = new PostiStadio[totPosti];
+            vet = new int[totPosti];
+            /* Attivazione gestore principale */
+            t = new Thread(gestore);
+            t.Start(); 
+        }
+
+        private void gestore()
+        {
+            // Smistamento clienti in tutte le casse disponibili
+
+            setLabel(lblStatus, "Vendita in corso!"); 
+            /* 1. Assegnazione casuale cliente-biglietteria */
+            for(int i=0; i<cli.Length; i++)
+            {
+                cli[i].idCliente = (i + 1);
+                cli[i].idBiglietteria = rnd.Next(0, 4);
+                cli[i].acquistato = false; 
+            } // SIMULAZIONE DELLA CODA 
+            /* 2. Inizializzazione struttura stadio */
+            for(int i=0; i<postiStadio.Length; i++)
+            {
+                postiStadio[i].idPosto = (i + 1);
+                postiStadio[i].idCliente = 0;
+                postiStadio[i].occupato = false; 
+            }
+
+            /* 3. Attivo le biglietterie */
+            biglietteria = new Thread[BIGLIETTERIE]; 
+            for(int i=0; i<BIGLIETTERIE; i++)
+            {
+                biglietteria[i] = new Thread(smaltisciCoda);
+                biglietteria[i].Name = i.ToString(); 
+                biglietteria[i].Start(); 
+            }
+            /* 4.  */
+            for (int i = 0; i < BIGLIETTERIE; i++)
+            {
+                biglietteria[i].Join();
+            }
+            setLabel(lblStatus, "Fine Vendita");
+            stampaFile();
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.Write((i+1).ToString() + ": ");
+                Console.WriteLine(vet[i]);
+            }
+        }
+
+        private void smaltisciCoda()
+        {
+            /* 4 Thread accedono a questa sezione 
+             * 1. Individuare punto d'uscita 
+             * 2. Individuare sezioni critiche e accessi a medesime risorse 
+             */
+            int posEstratto = 0;
+            int idBiglietteria = Convert.ToInt32(Thread.CurrentThread.Name);
+            if (getPostiStadioDisponibili() > 0 && getClientiDisponibili(idBiglietteria) > 0)
+            {
+                // Procedo con l'estrazione di un cliente (appartenente alla mia cassa) e procedo con il tentativo di acquisto
+                do
+                {
+                    posEstratto = rnd.Next(0, cli.Length);
+
+
+                } while (cli[posEstratto].idBiglietteria != idBiglietteria ||
+                          cli[posEstratto].smaltito == true);
+                // Ho in pos la posizione estratta per la cassa attuale
+                acquistaBiglietto(posEstratto);
+                Console.WriteLine(cli[posEstratto].idBiglietteria.ToString() + " " +
+                                    cli[posEstratto].idCliente.ToString());
+                Thread.Sleep(1000);
+
+            }
+            setLabel(labels[idBiglietteria], getClientiDisponibili(idBiglietteria).ToString());
+            setLabel(labelPosti, (getPostiStadioDisponibili() -1).ToString());
+            if (getPostiStadioDisponibili() > 1 && getClientiDisponibili(idBiglietteria) > 0)
+            {
+                smaltisciCoda();
+            }
+            else
+            {
+                setLabel(labels[idBiglietteria], "0");
+            }
+        }
+        private int getPostiStadioDisponibili()
+        {
+            lock (lock_campo)
+            {
+                int cnt = 0;
+                for (int i = 0; i < postiStadio.Length; i++)
+                    if (postiStadio[i].idCliente == 0)
+                        cnt++;
+                return cnt;
+            }
+        }
+        private int getClientiDisponibili(int cassa)
+        {
+            int cnt = 0;
+            for (int i = 0; i < cli.Length; i++)
+                if (cli[i].idBiglietteria == cassa && cli[i].smaltito == false)
+                    cnt++;
+            return cnt;
+        }
+        private int getClientiMorti()
+        {
+            int cnt = 0;
+            for (int i = 0; i < cli.Length; i++)
+                if (cli[i].smaltito == false)
+                    cnt++;
+            return cnt;
+        }
+        private void acquistaBiglietto(int pos)
+        {// 1. Il cliente esegue richiesta di acquisto biglietto
+         // 2. Se il posto che si vuole acquistatare è attualmente occupato o già acquistato si prendono strade diverse (es. posto occupato: attendo qualche secondo | posto già acquistato: cerco un'altra postazione o termino 
+         // 3. Avvio procedura di vendita: la vendita va a buon fine con una prob. del 75%
+         // SE Biglietto VENDUTO
+         // SE Biglietto NON VENDUTO
+            string aus = null;
+            int nBiglietti = rnd.Next(1, 6);
+            if (getPostiStadioDisponibili() > nBiglietti) {
+                for (int i = 0; i < nBiglietti; i++)
+                {
+                    int rndPos;
+                    do
+                    {
+                        rndPos = rnd.Next(0, totPosti);
+                    } while (postiStadio[rndPos].occupato == true);
+                    //nel caso sia disponibile il posto
+
+                    //postiStadio[rndPos].idPosto.ToString() + "\n"
+                    
+
+                    lock (lock_biglietti)
+                    {
+                        
+                        postiStadio[rndPos].occupato = true;
+                        //probabilità acquisto 75%
+                        int prob = rnd.Next(0, 4);
+                        if (prob != 1)
+                        {
+                            vet[rndPos]++;
+                            if (aus != null)
+                            {
+                                aus += ", " + postiStadio[rndPos].idPosto.ToString();
+                            }
+                            else
+                            {
+                                aus = postiStadio[rndPos].idPosto.ToString();
+                            }
+                            cli[pos].acquistato = true;
+                            postiStadio[rndPos].idCliente = cli[pos].idCliente;
+                            
+                        }
+                        else
+                        {
+                            postiStadio[rndPos].occupato = false;
+
+                        }
+                    }
+                    //Console.WriteLine("Il cliente "+ cl.idCliente + " ha acquistato il posto" + postiStadio[rndPos].idPosto + " // " + contClientiSmaltiti);
+                }
+            }
+            else
+            {
+                acquistaBiglietto(pos);
+            }
+            if (aus != null)
+            {
+                log[cli[pos].idBiglietteria] += "Cliente: " + cli[pos].idCliente + " ha acquistato un biglietto in posizione: " + aus + "\n";
+            }
+            cli[pos].smaltito = true;
+        }
+
+
+        private void scrivoFile(string fn, int index)
+        {
+            StreamWriter sw;
+            DateTime val;
+            val = DateTime.Now;
+            sw = new StreamWriter(fn+".txt", true);
+            sw.WriteLine("\n"+"Fine vendita biglietti: " + val.ToString() +"\n", true);
+            sw.WriteLine(log[index]);
+            sw.Flush(); //svuoto buffer
+            sw.Close();
+        }
+
+        private void lblClick(object sender, EventArgs e)
+        {
+            //Label lbl = sender as Label;
+            Label lbl = (Label)sender;
+            int aus = Convert.ToInt32(lbl.Name.Substring(3));
+
+            MessageBox.Show(log[aus]);
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isAttentato = true;
+            for (int i = 0; i < BIGLIETTERIE; i++)
+            {
+                biglietteria[i].Abort();
+            }
+        }
+
+        private void stampaFile()
+        {
+            if (isAttentato == false)
+            {
+                MessageBox.Show("E' possibile visualizzare i log di ogni cassa. Inoltre i vari dati verranno scaricati anche localmente");
+            }
+            else
+            {
+                MessageBox.Show("ATTENTATO IN CORSO!" + "\n" + "HANNO PERSO LA VITA: " + getClientiMorti().ToString());
+
+            }
+            scrivoFile("nord", 0);
+            scrivoFile("sud", 1);
+            scrivoFile("est", 2);
+            scrivoFile("ovest", 3);
+        }
+
+        private void setLabel(Label lbl, string msg)
+        {
+            BeginInvoke((MethodInvoker)delegate () {
+                lbl.Text = msg; 
+            }); 
+        }
+        
+    }
+}
