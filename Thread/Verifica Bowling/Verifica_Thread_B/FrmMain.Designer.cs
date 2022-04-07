aa784362d33d09d86e8d2235b42be2787da81b04
Gabriele <marellogabriele@gmail.com>
2022-04-07 13:22:50 +0200

    Upload correzione della verifica bowling
    Esercitazione relativa ad una sfida di bowling, svolta da batterie composte da 4 giocatori. Ognuno aveva 2 tiri disponibili per totalizzare il punteggio più alto. Il numero di giocatori veniva deciso dall'utente

---
+++ Thread/Verifica Bowling/Verifica_Thread_B/FrmMain.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Verifica_Thread_B
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nUDpartecipanti = new System.Windows.Forms.NumericUpDown();
+            this.btnAvvia = new System.Windows.Forms.Button();
+            this.txtPunteggio = new System.Windows.Forms.TextBox();
+            this.lblEsito = new System.Windows.Forms.Label();
+            this.picBG = new System.Windows.Forms.PictureBox();
+            this.lblPunti4 = new System.Windows.Forms.Label();
+            this.lblGioc4 = new System.Windows.Forms.Label();
+            this.picB4 = new System.Windows.Forms.PictureBox();
+            this.lblPunti3 = new System.Windows.Forms.Label();
+            this.lblGioc3 = new System.Windows.Forms.Label();
+            this.picB3 = new System.Windows.Forms.PictureBox();
+            this.lblPunti2 = new System.Windows.Forms.Label();
+            this.lblGioc2 = new System.Windows.Forms.Label();
+            this.picB2 = new System.Windows.Forms.PictureBox();
+            this.lblPunti1 = new System.Windows.Forms.Label();
+            this.lblGioc1 = new System.Windows.Forms.Label();
+            this.picB1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nUDpartecipanti)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.nUDpartecipanti);
+            this.groupBox1.Location = new System.Drawing.Point(605, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(217, 63);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Partecipanti";
+            // 
+            // nUDpartecipanti
+            // 
+            this.nUDpartecipanti.Location = new System.Drawing.Point(49, 22);
+            this.nUDpartecipanti.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nUDpartecipanti.Name = "nUDpartecipanti";
+            this.nUDpartecipanti.Size = new System.Drawing.Size(120, 23);
+            this.nUDpartecipanti.TabIndex = 0;
+            this.nUDpartecipanti.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nUDpartecipanti.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnAvvia
+            // 
+            this.btnAvvia.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAvvia.Location = new System.Drawing.Point(605, 81);
+            this.btnAvvia.Name = "btnAvvia";
+            this.btnAvvia.Size = new System.Drawing.Size(217, 37);
+            this.btnAvvia.TabIndex = 20;
+            this.btnAvvia.Text = "Avvia Torneo";
+            this.btnAvvia.UseVisualStyleBackColor = true;
+            this.btnAvvia.Click += new System.EventHandler(this.btnAvvia_Click);
+            // 
+            // txtPunteggio
+            // 
+            this.txtPunteggio.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPunteggio.Location = new System.Drawing.Point(605, 188);
+            this.txtPunteggio.Multiline = true;
+            this.txtPunteggio.Name = "txtPunteggio";
+            this.txtPunteggio.Size = new System.Drawing.Size(217, 377);
+            this.txtPunteggio.TabIndex = 19;
+            // 
+            // lblEsito
+            // 
+            this.lblEsito.AutoSize = true;
+            this.lblEsito.Location = new System.Drawing.Point(602, 121);
+            this.lblEsito.Name = "lblEsito";
+            this.lblEsito.Size = new System.Drawing.Size(59, 16);
+            this.lblEsito.TabIndex = 18;
+            this.lblEsito.Text = "[lblEsito]";
+            // 
+            // picBG
+            // 
+            this.picBG.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBG.BackgroundImage")));
+            this.picBG.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picBG.Location = new System.Drawing.Point(12, 3);
+            this.picBG.Name = "picBG";
+            this.picBG.Size = new System.Drawing.Size(587, 562);
+            this.picBG.TabIndex = 17;
+            this.picBG.TabStop = false;
+            // 
+            // lblPunti4
+            // 
+            this.lblPunti4.AutoSize = true;
+            this.lblPunti4.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPunti4.Location = new System.Drawing.Point(464, 477);
+            this.lblPunti4.Name = "lblPunti4";
+            this.lblPunti4.Size = new System.Drawing.Size(84, 19);
+            this.lblPunti4.TabIndex = 33;
+            this.lblPunti4.Text = "[lblPunti4]";
+            // 
+            // lblGioc4
+            // 
+            this.lblGioc4.AutoSize = true;
+            this.lblGioc4.BackColor = System.Drawing.Color.PaleGreen;
+            this.lblGioc4.Location = new System.Drawing.Point(464, 433);
+            this.lblGioc4.Name = "lblGioc4";
+            this.lblGioc4.Size = new System.Drawing.Size(61, 16);
+            this.lblGioc4.TabIndex = 32;
+            this.lblGioc4.Text = "[lblGioc4]";
+            // 
+            // picB4
+            // 
+            this.picB4.Image = ((System.Drawing.Image)(resources.GetObject("picB4.Image")));
+            this.picB4.Location = new System.Drawing.Point(409, 477);
+            this.picB4.Name = "picB4";
+            this.picB4.Size = new System.Drawing.Size(40, 30);
+            this.picB4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picB4.TabIndex = 31;
+            this.picB4.TabStop = false;
+            // 
+            // lblPunti3
+            // 
+            this.lblPunti3.AutoSize = true;
+            this.lblPunti3.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPunti3.Location = new System.Drawing.Point(464, 329);
+            this.lblPunti3.Name = "lblPunti3";
+            this.lblPunti3.Size = new System.Drawing.Size(83, 19);
+            this.lblPunti3.TabIndex = 30;
+            this.lblPunti3.Text = "[lblPunti3]";
+            // 
+            // lblGioc3
+            // 
+            this.lblGioc3.AutoSize = true;
+            this.lblGioc3.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lblGioc3.Location = new System.Drawing.Point(464, 285);
+            this.lblGioc3.Name = "lblGioc3";
+            this.lblGioc3.Size = new System.Drawing.Size(61, 16);
+            this.lblGioc3.TabIndex = 29;
+            this.lblGioc3.Text = "[lblGioc3]";
+            // 
+            // picB3
+            // 
+            this.picB3.Image = ((System.Drawing.Image)(resources.GetObject("picB3.Image")));
+            this.picB3.Location = new System.Drawing.Point(409, 329);
+            this.picB3.Name = "picB3";
+            this.picB3.Size = new System.Drawing.Size(40, 30);
+            this.picB3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picB3.TabIndex = 28;
+            this.picB3.TabStop = false;
+            // 
+            // lblPunti2
+            // 
+            this.lblPunti2.AutoSize = true;
+            this.lblPunti2.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPunti2.Location = new System.Drawing.Point(464, 197);
+            this.lblPunti2.Name = "lblPunti2";
+            this.lblPunti2.Size = new System.Drawing.Size(83, 19);
+            this.lblPunti2.TabIndex = 27;
+            this.lblPunti2.Text = "[lblPunti2]";
+            // 
+            // lblGioc2
+            // 
+            this.lblGioc2.AutoSize = true;
+            this.lblGioc2.BackColor = System.Drawing.Color.Khaki;
+            this.lblGioc2.Location = new System.Drawing.Point(464, 153);
+            this.lblGioc2.Name = "lblGioc2";
+            this.lblGioc2.Size = new System.Drawing.Size(61, 16);
+            this.lblGioc2.TabIndex = 26;
+            this.lblGioc2.Text = "[lblGioc2]";
+            // 
+            // picB2
+            // 
+            this.picB2.Image = ((System.Drawing.Image)(resources.GetObject("picB2.Image")));
+            this.picB2.Location = new System.Drawing.Point(409, 197);
+            this.picB2.Name = "picB2";
+            this.picB2.Size = new System.Drawing.Size(40, 30);
+            this.picB2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picB2.TabIndex = 25;
+            this.picB2.TabStop = false;
+            // 
+            // lblPunti1
+            // 
+            this.lblPunti1.AutoSize = true;
+            this.lblPunti1.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPunti1.Location = new System.Drawing.Point(464, 57);
+            this.lblPunti1.Name = "lblPunti1";
+            this.lblPunti1.Size = new System.Drawing.Size(80, 19);
+            this.lblPunti1.TabIndex = 24;
+            this.lblPunti1.Text = "[lblPunti1]";
+            // 
+            // lblGioc1
+            // 
+            this.lblGioc1.AutoSize = true;
+            this.lblGioc1.BackColor = System.Drawing.Color.Coral;
+            this.lblGioc1.Location = new System.Drawing.Point(464, 12);
+            this.lblGioc1.Name = "lblGioc1";
+            this.lblGioc1.Size = new System.Drawing.Size(58, 16);
+            this.lblGioc1.TabIndex = 23;
+            this.lblGioc1.Text = "[lblGioc1]";
+            // 
+            // picB1
+            // 
+            this.picB1.Image = ((System.Drawing.Image)(resources.GetObject("picB1.Image")));
+            this.picB1.Location = new System.Drawing.Point(409, 57);
+            this.picB1.Name = "picB1";
+            this.picB1.Size = new System.Drawing.Size(40, 30);
+            this.picB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picB1.TabIndex = 22;
+            this.picB1.TabStop = false;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 570);
+            this.Controls.Add(this.lblPunti4);
+            this.Controls.Add(this.lblGioc4);
+            this.Controls.Add(this.picB4);
+            this.Controls.Add(this.lblPunti3);
+            this.Controls.Add(this.lblGioc3);
+            this.Controls.Add(this.picB3);
+            this.Controls.Add(this.lblPunti2);
+            this.Controls.Add(this.lblGioc2);
+            this.Controls.Add(this.picB2);
+            this.Controls.Add(this.lblPunti1);
+            this.Controls.Add(this.lblGioc1);
+            this.Controls.Add(this.picB1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnAvvia);
+            this.Controls.Add(this.txtPunteggio);
+            this.Controls.Add(this.lblEsito);
+            this.Controls.Add(this.picBG);
+            this.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Torneo Bowling";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nUDpartecipanti)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown nUDpartecipanti;
+        private System.Windows.Forms.Button btnAvvia;
+        private System.Windows.Forms.TextBox txtPunteggio;
+        private System.Windows.Forms.Label lblEsito;
+        private System.Windows.Forms.PictureBox picBG;
+        private System.Windows.Forms.Label lblPunti4;
+        private System.Windows.Forms.Label lblGioc4;
+        private System.Windows.Forms.PictureBox picB4;
+        private System.Windows.Forms.Label lblPunti3;
+        private System.Windows.Forms.Label lblGioc3;
+        private System.Windows.Forms.PictureBox picB3;
+        private System.Windows.Forms.Label lblPunti2;
+        private System.Windows.Forms.Label lblGioc2;
+        private System.Windows.Forms.PictureBox picB2;
+        private System.Windows.Forms.Label lblPunti1;
+        private System.Windows.Forms.Label lblGioc1;
+        private System.Windows.Forms.PictureBox picB1;
+    }
+}
+
---
+++ Thread/Verifica Bowling/Verifica_Thread_B/FrmMain.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Verifica_Thread_B
+{
+    public partial class FrmMain : Form
+    {
+        const int PALLA_INIZIO_X = 400;
+        const int PALLA_FINE_X = 20;
+        const int TIRI = 2;
+        const int CORSIE = 4;
+        object lock_campo = new object(); 
+        volatile Random rnd; 
+        volatile Dictionary<string, int> giocatori = new Dictionary<string, int>();
+        volatile int totaleGiocatori; // Totale giocatori presenti nel file 
+        volatile int nGioc; // Giocatori Effettivi 
+        volatile int[] puntiGiocatori; // Array parallelo al dictionary per gestire il punteggio 
+        Thread arbitro;
+        Thread[] thArbCampo; // Thread gestori del campo 
+        Thread[] thCampo; // Giocatore impegnato nella gara 
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("giocatori.txt");
+            int cnt = 0;
+            while (!sr.EndOfStream)
+            {
+                //sr.ReadLine();
+                giocatori.Add(sr.ReadLine(), 0); // Tutti i giocatori presenti nel file 
+                cnt++;
+            }
+            sr.Close();
+            nUDpartecipanti.Maximum = cnt; // Massimo numero numericUpDown
+            puntiGiocatori = new int[cnt]; 
+            totaleGiocatori = cnt; 
+        }
+
+        private void btnAvvia_Click(object sender, EventArgs e)
+        {
+            rnd = new Random();
+            /* Recupero partecipanti effettivi */
+            nGioc = Convert.ToInt32(nUDpartecipanti.Value);
+            /* Avvio arbitro : main thread */
+            arbitro = new Thread(gestoreTorneo); 
+            arbitro.Start();
+        }
+
+        private void gestoreTorneo()
+        {
+            thArbCampo = new Thread[CORSIE];
+            thCampo = new Thread[CORSIE]; 
+
+            int posGioc; // Posizione del giocatore estratto 
+            // Estraggo giocatori partecipanti al torneo 
+            for(int i=0; i<nGioc; i++)
+            {
+                do
+                {
+                    posGioc = rnd.Next(0, totaleGiocatori); 
+                } while (giocatori.ElementAt(posGioc).Value == 1);
+                giocatori[giocatori.ElementAt(posGioc).Key] = 1; 
+            }
+            stampaPunteggio(); 
+            // Configurazione ed avvio thread gestori dei campi 
+            for(int i=0; i<CORSIE; i++)
+            {
+                thArbCampo[i] = new Thread(gestisciCampo);
+                thArbCampo[i].Name = (i + 1).ToString(); // 1, 2, 3, 4 
+                thArbCampo[i].Start(); // 4 Thread avviati 
+            }
+            
+            for (int i = 0; i < CORSIE; i++)
+            {
+                thArbCampo[i].Join();
+                Console.WriteLine("FINE GESTORE CAMPO " + (i + 1).ToString()); 
+            }
+            // Calcolo del vincitore 
+            setLabel(lblEsito, ""); 
+            for(int i=0; i<puntiGiocatori.Length; i++)
+            {
+                if (puntiGiocatori[i] == puntiGiocatori.Max())
+                    setLabel(lblEsito, lblEsito.Text +
+                        Environment.NewLine +
+                        giocatori.ElementAt(i).Key);
+            }
+        }
+
+        private void gestisciCampo()
+        {
+            // 4 Thread eseguono le istruzioni successive 
+            int posGioc;
+            Label[] lblGioc;
+            PictureBox picGioc;
+            Control[] parametri = new Control[3]; 
+
+            Thread.Sleep(100); 
+            lock (lock_campo)
+            {
+                if (giocatoriResidui() > 0)
+                {
+                    do
+                    {
+                        posGioc = rnd.Next(0, totaleGiocatori); 
+                    } while (giocatori.ElementAt(posGioc).Value != 1);
+                    giocatori[giocatori.ElementAt(posGioc).Key] = 0;
+                    thCampo[Convert.ToInt32(Thread.CurrentThread.Name) - 1] = new Thread(eseguiPartita);
+                    lblGioc = new Label[2]; 
+                    picGioc = new PictureBox();
+                    lblGioc[0] = (Label)Controls["lblGioc" + (Thread.CurrentThread.Name)];
+                    setLabel(lblGioc[0], giocatori.ElementAt(posGioc).Key);
+                    Thread.Sleep(300);
+                    lblGioc[1] = (Label)Controls["lblPunti" + (Thread.CurrentThread.Name)];
+                    picGioc = (PictureBox)Controls["picB" + (Thread.CurrentThread.Name)];
+                    parametri[0] = lblGioc[0]; // NomeGiocatore 
+                    parametri[1] = lblGioc[1];  // Punti
+                    parametri[2] = picGioc;
+
+                    thCampo[Convert.ToInt32(Thread.CurrentThread.Name) - 1].Name = posGioc.ToString();
+                    thCampo[Convert.ToInt32(Thread.CurrentThread.Name) - 1].Start(parametri); 
+                }
+            }// Accesso bloccato 
+            thCampo[Convert.ToInt32(Thread.CurrentThread.Name) - 1].Join(); 
+            // join 
+            if (giocatoriResidui() > 0)
+                gestisciCampo(); 
+        }
+
+        private void eseguiPartita(object par)
+        {
+            Label[] lbl = new Label[2]; 
+            PictureBox pic = new PictureBox();
+            int punteggio;
+            int posGioc; 
+            lbl[0] = (Label)(par as Control[])[0];
+            lbl[1] = (Label)(par as Control[])[1]; 
+            pic = (PictureBox)(par as Control[])[2];
+
+            for(int i=0; i<TIRI; i++)
+            {
+                do
+                {
+                    setPos(pic, pic.Location.X - rnd.Next(10, 20), pic.Location.Y);
+                    Thread.Sleep(rnd.Next(10, 50)); 
+                } while (pic.Location.X >= PALLA_FINE_X);
+                setPos(pic, PALLA_INIZIO_X, pic.Location.Y);
+                /* Calcolo punteggio */
+                punteggio = rnd.Next(0, 10);
+                posGioc = Convert.ToInt32(Thread.CurrentThread.Name);
+                puntiGiocatori[posGioc] += punteggio; 
+
+                setLabel(lbl[1], "Punti: " + puntiGiocatori[posGioc].ToString());
+            }
+            stampaPunteggio();
+        }
+
+        private void setPos(PictureBox pic, int x, int y)
+        {
+            BeginInvoke((MethodInvoker)delegate ()
+            {
+                pic.Location = new Point(x, y);
+            });
+        }
+
+        private void setLabel(Label lbl, string msg)
+        {
+            BeginInvoke((MethodInvoker)delegate ()
+            {
+                lbl.Text = msg;
+            }); 
+        }
+
+        private int giocatoriResidui()
+        {
+            int giocRes = 0;
+            for (int i = 0; i < giocatori.Count; i++) 
+            {
+                if (giocatori.ElementAt(i).Value == 1)
+                    giocRes++; 
+            }
+            return giocRes; 
+        }
+        private void stampaPunteggio()
+        {
+            BeginInvoke((MethodInvoker)delegate () {
+                txtPunteggio.Text = ""; 
+                for(int i=0; i<giocatori.Count; i++)
+                {
+                    txtPunteggio.Text += "[" + giocatori.ElementAt(i).Value + "]" +
+                                giocatori.ElementAt(i).Key + ": " + puntiGiocatori[i].ToString();
+                    txtPunteggio.Text += Environment.NewLine; 
+                }
+            }); 
+        }
+    }
+}
